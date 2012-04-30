ef639f7abd4bf0dee861ba927d9dc73a142449a4
Evan Kuchar <evan.kuchar@gmail.com>
2012-04-29 20:41:21 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerlog::Application.config.secret_token = '5d25e070739bf77977218856915f940c7eb0499ac7f6b6cbbd31aca25c55425849cca4c09ffa7ae02a90f1663857024d3ca6cc00a3a03b1a2bd40605e4ecbcb9'
